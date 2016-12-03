68a9ec2659e7090674ef02b7fa27bcf4c3cb9314
Raul Carlos de Mello <raul@localhost.localdomain>
2016-12-03 07:36:54 -0200

    kivy-camera

---
+++ main.py
@@ -0,0 +1,50 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.image import Image
+from kivy.core.window import Window
+
+
+kv = '''
+BoxLayout:
+	orientation: 'vertical'
+	Camera:
+		id: camera
+		resolution: 1920, 1080
+	BoxLayout:
+		orientation: 'horizontal'
+		size_hint_y: None
+		height: '48dp'
+		Button:
+			markup: True
+			text: '[b][color=#0000]start[/b][/color]'
+			on_release: camera.play = True
+		Button:
+			text: 'Stop'
+			on_release:
+				camera.play = False
+				app.save_picture(camera.texture)
+		Button:
+			text: 'PrintScr'
+			on_release: app.print_scr()
+'''
+
+
+class CameraApp(App):
+	fileprefix = 'snap'
+	
+	def build(self):
+		return Builder.load_string(kv)
+
+	def save_picture(self, picture):
+		img = Image(picture)
+		img.save('eu3.png')
+	
+	def print_scr(self, *largs):
+		outname = self.fileprefix+'_%(counter)04d.png'
+		Window.screenshot(name=outname)
+	
+if __name__ == '__main__':
+	CameraApp().run()
+
+
+
